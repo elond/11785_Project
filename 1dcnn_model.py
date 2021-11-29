7d518ec324216586bd3d93fd45e82b7bcf3dd42f
elond <52602172+elond@users.noreply.github.com>
2021-11-29 14:03:51 -0500

    Add files via upload

--- 1dcnn_model.py
+++ 1dcnn_model.py
@@ -15,16 +15,23 @@ import sys
 ################################################################################
 #--------Directories, Data Titles, and Conditionals ---------------------------#
 ################################################################################
-INPUT_DIR = "/home/esteban/Documents/Research/Data/All_Viruses/"
-OUTPUT_DIR = "/home/esteban/Documents/Research/Data/Results/"
-TRAIN_DATA_FILE = INPUT_DIR + "trainingReads/strain_run_train_data.npy"
-TRAIN_LABEL_FILE = INPUT_DIR + "trainingReads/strain_run_train_labels.npy"
-VAL_DATA_FILE = INPUT_DIR + "validationReads/strain_run_valid_data.npy"
-VAL_LABEL_FILE = INPUT_DIR + "validationReads/strain_run_valid_labels.npy"
-TEST_DATA_FILE = INPUT_DIR + "testReads/strain_run_test_data.npy"
-TEST_LABEL_FILE = INPUT_DIR + "testReads/strain_run_test_labels.npy"
+train_d = "GenBank/" #or "RefSeq/"
+test_d = "RefSeq/" #or "GenBank/"
+encoding_type = "CAT" # or "One_hot"
+input_rows = 21 # 4 if One_hot and 21 if CAT
+input_len = 83 # 250 if One_hot and 83 if CAT
+MODEL_NAME = "{}".format(encoding_type)
+INPUT_TRAIN_DIR = "/home/esteban/Documents/School/Class_11785/Project/Data/NPY_Data/" + train_d + encoding_type +"/"
+INPUT_TEST_DIR = "/home/esteban/Documents/School/Class_11785/Project/Data/NPY_Data/" + test_d + encoding_type +"/"
+OUTPUT_DIR = "/home/esteban/Documents/School/Class_11785/Project/Data/NPY_Data/Results/"
+TRAIN_DATA_FILE = INPUT_TRAIN_DIR + "train_data.npy"
+TRAIN_LABEL_FILE = INPUT_TRAIN_DIR + "train_labels.npy"
+VAL_DATA_FILE = INPUT_TRAIN_DIR + "val_data.npy"
+VAL_LABEL_FILE = INPUT_TRAIN_DIR + "val_labels.npy"
+TEST_DATA_FILE = INPUT_TEST_DIR + "val_data.npy"
+TEST_LABEL_FILE = INPUT_TEST_DIR + "val_labels.npy"
 
-PPPE = 100                                                                       # How many plot points per epoch do you want?
+PPPE = 10                                                                       # How many plot points per epoch do you want?
 NUM_DATA = 5                                                                    # Number of runs through the same architecture
 FIRST_LAYER_RANGE = [21]
 SECOND_LAYER_RANGE = [16,32,64,128,256]
@@ -41,7 +48,7 @@ DEVICE = torch.device(DEV)
 LR_LIST = [0.001]                                                               # What learning rate do you want?
 EPOCH_NUM = 10                                                                  # For how many epochs do you want to run the data?
 BATCH_SIZE_LIST = [128]                                                         # How big is the mini-batch size?
-window = 3
+window = 1 # 3 if one_hot and 1 if CAT
 PADDING_LIST = 0
 ################################################################################
 #--------Tracking and Saving Code----------------------------------------------#
@@ -92,29 +99,34 @@ class ProgressMeter(object):
 #--------Network Archictecture-------------------------------------------------#
 ################################################################################
 class Net(nn.Module):
-    def __init__(self, conv2_hu, conv3_hu, window):
+    def __init__(self, input_rows, input_len, conv1_hu, conv2_hu, conv3_hu, window):
         super(Net, self).__init__()
 
-        conv_input_rows = 250
-        conv_input_col = 4
-        conv1_hu = 21
+        conv_input_rows = input_len # 250 for One_hot 83 for CAT
+        conv_input_col = 3 # 4 for One_hot and 3 if CAT
         stride = 1
         padding = 0
-        self.conv1 = nn.Conv1d(conv_input_col,conv1_hu,window,stride,padding)
+        """ self.conv1 = nn.Conv1d(conv_input_col,conv1_hu,window,stride,padding)
         self.out_dim = int((conv_input_rows + 2*padding - window)/stride + 1)
         self.conv2 = nn.Conv1d(conv1_hu, conv2_hu,window,stride)
         self.out_dim = int((self.out_dim - window)/stride + 1)
         self.conv3 = nn.Conv1d(conv2_hu, conv3_hu, window, stride)
+        self.out_dim = int((self.out_dim - window)/stride + 1)"""
+        self.conv1 = nn.Conv2d(conv_input_col,conv1_hu,(21,window),stride,padding)
+        self.out_dim = int((conv_input_rows + 2*padding - window)/stride + 1)
+        self.conv2 = nn.Conv2d(conv1_hu, conv2_hu,window,stride)
+        self.out_dim = int((self.out_dim - window)/stride + 1)
+        self.conv3 = nn.Conv2d(conv2_hu, conv3_hu, window, stride)
         self.out_dim = int((self.out_dim - window)/stride + 1)
 
         self.fc1 = nn.Linear(self.out_dim*conv3_hu, 8)
 
     def forward(self, x):
         x = x.float()
-        x = m(F.relu(self.conv1(x)))
-        x = m(F.relu(self.conv2(x)))
-        x = m(F.relu(self.conv3(x)))
-        x = torch.flatten(x)
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = torch.flatten(x, start_dim=1)
         x = self.fc1(x)
         return x
 
@@ -123,9 +135,18 @@ class Net(nn.Module):
 ################################################################################
 class LoadData(Dataset):
     def __init__(self, data, labels):
-        data_x = np.load(data)                                                  # Load the examples-------------#
+        label_dict = {"data":0,"Adenovirus":1,"Astrovirus":2,"Calicivirus":3,"Enterovirus":4,
+                      "Hepatitis_A": 5, "Hepatitis_E":6, "Rotavirus":7,"Orthoreovirus":8}
+        with open(data, mode='rb') as f:
+            fsz = os.fstat(f.fileno()).st_size
+            data_x = np.load(f)
+            i = 0
+            while f.tell() < fsz:
+                data_x = np.vstack((data_x, np.load(f)))                                                  # Load the examples-------------#
+                i+=1
         data_y = np.load(labels)                                                # Load the labels---------------#
-        self.data = torch.from_numpy(data_x).permute(0,2,1)                     # Convert examples to tensor----#
+        #data_y = np.array([label_dict[species] for species in data_y.tolist()])
+        self.data = torch.from_numpy(data_x).permute(0,1,3,2)                     # Convert examples to tensor----# (This should be permute(0,2,1) for One_hot)
         self.labels = torch.from_numpy(data_y.astype(int))                      # Convert labels to tensor------#
         self.len = len(data_y)                                                  # Determine length of dataset---#
 
@@ -199,8 +220,9 @@ def RunDataset(loader, ml_data, device, net, csv_writer,network_name,fig_dir,t_t
         ac = (tp+tn) / (tn+tp+fp+fn)                                                # accuracy-------------------------------#
         sp = tn/(tn+fp)                                                             # specifcity, tnr------------------------#
         f1 = 2*pr*rc/(pr+rc)                                                        # f1 score-------------------------------#
-        fpr, tpr, thresholds = roc_curve(labels_list, prob_list)                    # roc curve metrics----------------------#
-        roc_auc = roc_auc_score(labels_list, prob_list)                             # auroc----------------------------------#
+        fpr = fp/(fp+tn)
+        tpr = tp/(tp+fn)
+        roc_auc = roc_auc_score(labels_list, prob_list, multi_class="ovr")          # auroc----------------------------------#
         run_time = time.time()-start_time                                           # run time-------------------------------#
         data_list = [tp, fp, tn, fn, ac, rc, pr, sp, f1, roc_auc, run_time, t_time] # classification metric summary----------#
 
@@ -221,16 +243,17 @@ def RunDataset(loader, ml_data, device, net, csv_writer,network_name,fig_dir,t_t
         #--------output csv----------------------------------------------------#
         csv_writer.writerow(hyp_par + [ml_data] + data_list)
 
-file_name = "{}FirstPaper_deep_3_model_results.csv".format(OUTPUT_DIR)
+file_name = "{}{}_results.csv".format(OUTPUT_DIR,MODEL_NAME)
 with open(file_name, mode="a+") as output_csv:
     CSV_WRITER = csv.writer(output_csv, delimiter=',')
-    CSV_WRITER.writerow(["1st Hidden Units", "2nd Hidden Units", "Window Size",
-                         "Padding", "Batch Size", "LR", "Epochs", "ML_Dataset",
-                         "True Positives", "False Positives", "True Negatives",
+    CSV_WRITER.writerow(["1st Hidden Units", "2nd Hidden Units", "3rd Hidden Units",
+                         "Window Size","Padding", "Batch Size", "LR", "Epochs",
+                         "ML_Dataset","True Positives", "False Positives","True Negatives",
                          "False Negatives", "Accuracy", "Recall", "Precision",
                          "Specificity", "F1 score", "AUROC", "Test Run Time (s)", "Train time (s)"])
 for x in range(NUM_DATA):
     for BATCH_SIZE in BATCH_SIZE_LIST:
+        print("-"*20,"Loading Data", "-"*20)
         TRAIN_DATASET = LoadData(TRAIN_DATA_FILE, TRAIN_LABEL_FILE)
         TRAIN_LOADER = DataLoader(dataset=TRAIN_DATASET, batch_size=BATCH_SIZE, shuffle=True)
         VALID_DATA = LoadData(VAL_DATA_FILE, VAL_LABEL_FILE)
@@ -244,56 +267,57 @@ for x in range(NUM_DATA):
             sys.exit()
 
         for LEARNING_RATE in LR_LIST:
-            NETWORK_NAME = "3CNN2FC_BS{}_LR{}_Padding{}_W{}".format(BATCH_SIZE,LEARNING_RATE,PADDING_LIST,window)
+            NETWORK_NAME = "{}_BS{}_LR{}_Padding{}_W{}".format(MODEL_NAME,BATCH_SIZE,LEARNING_RATE,PADDING_LIST,window)
             FIGURE_DIR = OUTPUT_DIR + "Figures/" + NETWORK_NAME + "/"
             if os.path.isdir(FIGURE_DIR) == False:
                 print("Making directory " + FIGURE_DIR)
                 os.mkdir(FIGURE_DIR)
 
-            for sec_cnn_hu in SECOND_LAYER_RANGE:
-                for thir_cnn_hu in THIRD_LAYER_RANGE:
-                    NETWORK = Net(sec_cnn_hu,thir_cnn_hu,window).to(DEVICE)
-                    NETWORK.apply(weights_init)
-                    LOSS_FUNCTION = nn.CrossEntropyLoss()
-                    OPTIMIZER = optim.Adam(NETWORK.parameters(), lr=LEARNING_RATE)
-                    ################################################################################
-                    #--------Train the model-------------------------------------------------------#
-                    ################################################################################
-                    FIGURE_NAME = FIGURE_DIR + "{}_1_{}_2_{}_LogLoss.png".format(NETWORK_NAME,str(sec_cnn_hu),str(thir_cnn_hu))
-                    TRAIN_MOV_AVG =[]
-                    VAL_MOV_AVG = []
-                    RUN_LOS_INTERVAL = int(len(TRAIN_LOADER)//PPPE)
-                    START_TIME = time.time()
-                    TRAIN_LOSS = AverageMeter('Train Loss', ':.4e')
-                    VAL_LOSS = AverageMeter('Val Loss', ':.4e')
-                    for EPOCH in range(1, EPOCH_NUM+1):
-                        TEMP_MOV_T, TEMP_MOV_V = TrainModel(TRAIN_LOADER, VAL_LOADER, NETWORK, OPTIMIZER, DEVICE, EPOCH, LOSS_FUNCTION, RUN_LOS_INTERVAL, TRAIN_LOSS, VAL_LOSS)
-                        TRAIN_MOV_AVG += TEMP_MOV_T
-                        VAL_MOV_AVG += TEMP_MOV_V
+            for fir_cnn_hu in FIRST_LAYER_RANGE:
+                for sec_cnn_hu in SECOND_LAYER_RANGE:
+                    for thir_cnn_hu in THIRD_LAYER_RANGE:
+                        NETWORK = Net(input_rows, input_len,fir_cnn_hu,sec_cnn_hu,thir_cnn_hu,window).to(DEVICE)
+                        NETWORK.apply(weights_init)
+                        LOSS_FUNCTION = nn.CrossEntropyLoss()
+                        OPTIMIZER = optim.Adam(NETWORK.parameters(), lr=LEARNING_RATE)
+                        ################################################################################
+                        #--------Train the model-------------------------------------------------------#
+                        ################################################################################
+                        FIGURE_NAME = FIGURE_DIR + "{}_2_{}_3_{}_LogLoss.png".format(NETWORK_NAME,str(sec_cnn_hu),str(thir_cnn_hu))
+                        TRAIN_MOV_AVG =[]
+                        VAL_MOV_AVG = []
+                        RUN_LOS_INTERVAL = int(len(TRAIN_LOADER)//PPPE)
+                        START_TIME = time.time()
+                        TRAIN_LOSS = AverageMeter('Train Loss', ':.4e')
+                        VAL_LOSS = AverageMeter('Val Loss', ':.4e')
+                        for EPOCH in range(1, EPOCH_NUM+1):
+                            TEMP_MOV_T, TEMP_MOV_V = TrainModel(TRAIN_LOADER, VAL_LOADER, NETWORK, OPTIMIZER, DEVICE, EPOCH, LOSS_FUNCTION, RUN_LOS_INTERVAL, TRAIN_LOSS, VAL_LOSS)
+                            TRAIN_MOV_AVG += TEMP_MOV_T
+                            VAL_MOV_AVG += TEMP_MOV_V
 
-                    TRAIN_TIME = str((time.time() - START_TIME))
-                    print("Training Time: " + TRAIN_TIME)
-                    print('Finished Training')
-                    ################################################################################
-                    #--------Plot the training loss over time--------------------------------------#
-                    ################################################################################
-                    MINI_BATCH_LIST = range(1, len(TRAIN_MOV_AVG)+1)
-                    plt.figure()
-                    plt.plot(MINI_BATCH_LIST, TRAIN_MOV_AVG, label="50-Data Training loss")
-                    plt.plot(MINI_BATCH_LIST, VAL_MOV_AVG, label="50-Data Validation loss")
-                    plt.legend()
-                    plt.xlabel("Training examples")
-                    plt.ylabel("Loss")
-                    plt.title("Loss over training")
-                    plt.savefig(FIGURE_NAME)
-                    plt.close()
-                    ################################################################################
-                    #--------Test the model--------------------------------------------------------#
-                    ################################################################################
-                    with open(file_name, mode="a+") as output_csv:
-                        CSV_WRITER = csv.writer(output_csv, delimiter=',')
-                        DATA_LIST = {"Train": TRAIN_LOADER, "Validation": VAL_LOADER, "Test": TEST_LOADER}
-                        H_P_LIST = [sec_cnn_hu, thir_cnn_hu, window, PADDING_LIST, BATCH_SIZE, LEARNING_RATE, EPOCH_NUM]
-                        for DATA_SET in DATA_LIST:
-                            print("Testing {} dataset...".format(DATA_SET))
-                            RunDataset(DATA_LIST[DATA_SET],DATA_SET,DEVICE,NETWORK,CSV_WRITER,"{}_1_{}_2_{}_ROC".format(NETWORK_NAME,str(sec_cnn_hu),str(thir_cnn_hu)),FIGURE_DIR,TRAIN_TIME,H_P_LIST)
+                        TRAIN_TIME = str((time.time() - START_TIME))
+                        print("Training Time: " + TRAIN_TIME)
+                        print('Finished Training')
+                        ################################################################################
+                        #--------Plot the training loss over time--------------------------------------#
+                        ################################################################################
+                        MINI_BATCH_LIST = range(1, len(TRAIN_MOV_AVG)+1)
+                        plt.figure()
+                        plt.plot(MINI_BATCH_LIST, TRAIN_MOV_AVG, label="50-Data Training loss")
+                        plt.plot(MINI_BATCH_LIST, VAL_MOV_AVG, label="50-Data Validation loss")
+                        plt.legend()
+                        plt.xlabel("Training examples")
+                        plt.ylabel("Loss")
+                        plt.title("Loss over training")
+                        plt.savefig(FIGURE_NAME)
+                        plt.close()
+                        ################################################################################
+                        #--------Test the model--------------------------------------------------------#
+                        ################################################################################
+                        with open(file_name, mode="a+") as output_csv:
+                            CSV_WRITER = csv.writer(output_csv, delimiter=',')
+                            DATA_LIST = {"Train": TRAIN_LOADER, "Validation": VAL_LOADER, "Test": TEST_LOADER}
+                            H_P_LIST = [fir_cnn_hu, sec_cnn_hu, thir_cnn_hu, window, PADDING_LIST, BATCH_SIZE, LEARNING_RATE, EPOCH_NUM]
+                            for DATA_SET in DATA_LIST:
+                                print("Testing {} dataset...".format(DATA_SET))
+                                RunDataset(DATA_LIST[DATA_SET],DATA_SET,DEVICE,NETWORK,CSV_WRITER,"{}_2_{}_3_{}_ROC".format(NETWORK_NAME,str(sec_cnn_hu),str(thir_cnn_hu)),FIGURE_DIR,TRAIN_TIME,H_P_LIST)
